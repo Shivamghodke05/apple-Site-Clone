caf4ced39fca058a3594384d22d177c8286bc931
shivamghodke05 <shivamghodke05@gmail.com>
2025-12-25 16:59:13 +0530

    implemented hero section

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div>
+        <h1>Macbook Pro</h1>
+        <img src="/title.png" alt="Macbook Title" />
+      </div>
+      <video src="/videos/hero.mp4" autoPlay muted playinline />
+          <button>Buy</button>
+          <p>From $1500 or $133/mo for 12 months</p>
+    </section>
+  );
+};
+
+export default Hero;
