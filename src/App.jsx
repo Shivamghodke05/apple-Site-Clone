3ee2f73bc58ee1162422643260ca2d8034fb2266
shivamghodke05 <shivamghodke05@gmail.com>
2025-12-25 22:44:42 +0530

    implemented lightformer and spotlights on an 3d model of  macbook 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NavBar from "./Components/NavBar.jsx";
 import Hero from "./Components/Hero.jsx";
-
+import ProductViewer from "./Components/ProductViewer.jsx";
 const App = () => {
   return (
       <main>
           <NavBar />
           <Hero />
+          <ProductViewer />
     </main>
   )
 }
